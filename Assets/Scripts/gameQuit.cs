7ca86b512110a6d29fa39f287c3698254e2dc68b
Natalie <maycry@gmx.de>
2016-10-09 01:05:09 +0200

    added pipes and Cube - agin

---
+++ Assets/Scripts/gameQuit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class gameQuit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        onCollisionEnter();
+	}
+
+    void onCollisionEnter()
+    {
+        Debug.Log("Collision Detected")
+        SceneManager.LoadScene(0);
+        
+    }
+
+}
