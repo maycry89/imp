ca45433a065587a0a03d7777f4e23f0aa079f181
Natalie <maycry@gmx.de>
2016-10-19 23:41:18 +0200

    Added random block spawning

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 public class Obstacle : MonoBehaviour {
 
     public Vector3 velocity = new Vector3(-4f, 0f, 0f);
-    
+    public float range = 4;
+
 	// Use this for initialization
 	void Start () {
         gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-4f, 0f, 0f);
-        transform
+        transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
     }
 	
 	// Update is called once per frame
